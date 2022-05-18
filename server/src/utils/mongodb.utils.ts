6afa6b1df6a79e65e7732d05d658b19c733e47c2
beez-dev <bishaljarnal456@gmail.com>
2022-05-18 09:52:00 +0545

    removed redundant file

--- server/src/utils/mongodb.utils.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-/**
- * Convert from string to mongo db ObjectId type
- */
-export function getMongoObjectId(id: string | mongoose.Types.ObjectId) {
-    return new mongoose.Types.ObjectId(id);
-}
